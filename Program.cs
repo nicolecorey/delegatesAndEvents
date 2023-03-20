4707f4e9ad6b3ab4b8eeb286e80b2df4ff81a5ce
mybil <mybil@NICOLE>
2023-03-20 12:18:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace delegatesAndEvents
+{
+    // create a delegate
+    public delegate void Ending(int n);
+    public class Race
+    {
+        public event Ending RaceEnd; // create a delegate event object
+
+        public void Racing(int contestants, int laps)
+        {
+            Console.WriteLine("Ready\nSet\nGo!");
+            Random r = new Random();
+            int[] participants = new int[contestants];
+            bool done = false;
+            int champ = -1;
+            // first to finish specified number of laps wins
+            while (!done)
+            {
+                for (int i = 0; i < contestants; i++)
+                {
+
+                    if (participants[i] <= laps)
+                    {
+                        participants[i] += r.Next(1, 5);
+                    }
+                    else
+                    {
+                        champ = i;
+                        done = true;
+                        continue;
+                    }
+                }
+
+            }
+
+            TheWinner(champ);
+        }
+        private void TheWinner(int champ)
+        {
+            Console.WriteLine("Woohoo! We have a winner!");
+            // invoke the delegate event object and pass champ to the method
+            RaceEnd(champ);
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            // create a class object
+            Race round1 = new Race();
+            // register with the footRace event
+            round1.RaceEnd += footRace;
+            // trigger the event
+            round1.Racing(20,4);
+            // register with the carRace event
+            round1.RaceEnd -= footRace;
+            round1.RaceEnd += carRace;
+            //trigger the event
+            round1.Racing(10,8);
+            // register with the bike race event using a lambda expression
+            round1.RaceEnd -= carRace;
+            round1.RaceEnd += (winner) => Console.WriteLine($"The winner of the race is bike {winner}!");
+            // trigger the event
+            round1.Racing(15,20);
+
+        }
+
+        // event handlers
+        public static void carRace(int winner)
+        {
+            Console.WriteLine($"Car number {winner} is the winner!");
+        }
+        public static void footRace(int winner)
+        {
+            Console.WriteLine($"Racer number {winner} is the winner!");
+        }
+    }
+}
